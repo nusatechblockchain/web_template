91ac8f5370ce80a67d81df7b801cbb63e99cf013
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2022-12-05 14:31:22 +0700

    on progress wallet

--- src/desktop/containers/Sidebar/index.tsx
+++ src/desktop/containers/Sidebar/index.tsx
@@ -16,6 +16,7 @@ import {
     UserIcon,
     WalletIcon,
 } from '../../../assets/images';
+import './Sidebar.pcss';
 
 const Sidebar = () => {
     const [menuActive, setMenuActive] = useState('Dashboard');
@@ -23,83 +24,75 @@ const Sidebar = () => {
     const data = [
         {
             name: 'Dashboard',
-            icon: <UserIcon strokeColor={menuActive === 'Dashboard' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <UserIcon strokeColor={menuActive === 'Dashboard' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/dashboard',
         },
         {
             name: 'Wallet',
-            icon: <WalletIcon fillColor={menuActive === 'Wallet' ? '#F2F0FF' : '#B5B3BC;'} />,
-            path: '/wallet',
+            icon: <WalletIcon fillColor={menuActive === 'Wallet' ? '#F2F0FF' : '#B5B3BC'} />,
+            path: '/wallets',
         },
         {
             name: 'Market Order',
-            icon: <AnalysIcon fillColor={menuActive === 'Market Order' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <AnalysIcon fillColor={menuActive === 'Market Order' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/market-order',
         },
         {
             name: 'Trade History',
-            icon: <CalendarIcon fillColor={menuActive === 'Trade History' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <CalendarIcon fillColor={menuActive === 'Trade History' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/trade-history',
         },
         {
             name: 'Profile Setting',
-            icon: <SettingIcon fillColor={menuActive === 'Profile Setting' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <SettingIcon fillColor={menuActive === 'Profile Setting' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/profile-setting',
         },
         {
             name: 'Security',
-            icon: <SecurityIcon fillColor={menuActive === 'Security' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <SecurityIcon fillColor={menuActive === 'Security' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/security',
         },
         {
             name: 'Referral',
-            icon: <AddUserIcon fillColor={menuActive === 'Referral' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <AddUserIcon fillColor={menuActive === 'Referral' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/referral',
         },
         {
             name: 'API Management',
-            icon: <ApiIcon fillColor={menuActive === 'API Management' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <ApiIcon fillColor={menuActive === 'API Management' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/api-management',
         },
         {
             name: 'Announcement',
-            icon: <AnnouncementIcon fillColor={menuActive === 'Announcement' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <AnnouncementIcon fillColor={menuActive === 'Announcement' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/announcement',
         },
         {
             name: 'FAQ',
-            icon: <FaqIcon fillColor={menuActive === 'FAQ' ? '#F2F0FF' : '#B5B3BC;'} />,
+            icon: <FaqIcon fillColor={menuActive === 'FAQ' ? '#F2F0FF' : '#B5B3BC'} />,
             path: '/faq',
         },
     ];
 
+    console.log(data.length);
+
     return (
-        <div className="sidebar dark-bg-accent">
+        <div className="sidebar dark-bg-main">
             <ul>
-                <li>
-                    <a href="../../Screen/HistoryTrade/index.html" className="d-flex">
-                        <img src="../../Assets/Icon/sidebar-overview.svg" alt="" />
-                        <p className="white-text font-bold text-sm pl-2 mb-0">Overview</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="../../Screen/Deposit/index.html" className="d-flex">
-                        <img src="../../Assets/Icon/sidebar-deposit.svg" alt="" />
-                        <p className="grey-text-accent font-bold text-sm pl-2 mb-0">Deposit</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="../../Screen/Withdrawl/index.html" className="d-flex">
-                        <img src="../../Assets/Icon/sidebar-withdraw.svg" alt="" />
-                        <p className="grey-text-accent font-bold text-sm pl-2 mb-0">Withdrawl</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="../../Screen/Deposit/index.html" className="d-flex">
-                        <img src="../../Assets/Icon/sidebar-internal.svg" alt="" />
-                        <p className="grey-text-accent font-bold text-sm pl-2 mb-0">Internal Transfer</p>
-                    </a>
-                </li>
+                {data &&
+                    data.map((el, i) => (
+                        // <Link key={i} to={`${el.path}`}>
+                        <li onClick={() => setMenuActive(el.name)} className="d-flex align-items-center cursor-pointer">
+                            {el.icon}
+                            <p
+                                className={`font-bold text-sm pl-2 mb-0 ${
+                                    menuActive === el.name ? 'white-text' : 'grey-text'
+                                }`}>
+                                {el.name}
+                            </p>
+                        </li>
+                        // </Link>
+                    ))}
             </ul>
             <div className="devider"></div>
         </div>
@@ -107,3 +100,4 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+//
