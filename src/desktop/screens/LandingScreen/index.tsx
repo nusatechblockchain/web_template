6aa9e28e92b39573a89d0abff62ff39e37f3b936
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-06 09:44:18 +0700

    hafidz - create landing page

--- src/desktop/screens/LandingScreen/index.tsx
+++ src/desktop/screens/LandingScreen/index.tsx
@@ -11,12 +11,23 @@ import { toggleColorTheme } from '../../../helpers';
 import { RootState, selectCurrentColorTheme, selectUserLoggedIn } from '../../../modules';
 import { BnbIcon, BtcIcon, DogeIcon, TronIcon } from '../../../assets/images/CoinIcon';
 import { ArrowDownLarge } from '../../../assets/images/ArrowDownIcon';
+import {
+    FeatureIcon1,
+    FeatureIcon2,
+    FeatureIcon3,
+    FeatureIcon4,
+    FeatureIcon5,
+    FeatureIcon6,
+} from '../../../assets/images/LandingFeatureIcon';
+import { AndroidIcon, AppleStoreIcon, GooglePlayIcon, MacOsIcon, WindowsIcon } from '../../../assets/images/DeviceIcon';
 import AnouncementIcon from '../../../assets/png/landing-announcement.png';
 import SliderBgImage from '../../../assets/png/bg-slider-item.png';
 import BitcoinIcon from '../../../assets/png/Bitcoin.png';
 import EtherumIcon from '../../../assets/png/Etherium.png';
 import LiteIcon from '../../../assets/png/LiteCoin.png';
 import MoneroIcon from '../../../assets/png/Monero.png';
+import MarketAnalysisImage from '../../../assets/png/image-blog.png';
+import LandingAplicationImage from '../../../assets/png/aplication.png';
 
 interface ReduxProps {
     isLoggedIn: boolean;
@@ -48,7 +59,6 @@ class Landing extends React.Component<Props> {
         return (
             <div className="landing-screen dark-bg-accent">
                 <div className="content-wrapper no-sidebar ">
-                    {/* hero */}
                     <section className="hero position-relative">
                         <img src={BitcoinIcon} className="image-coin btc" alt="" />
                         <img src={EtherumIcon} className="image-coin eth" alt="" />
@@ -143,9 +153,7 @@ class Landing extends React.Component<Props> {
                             </div>
                         </div>
                     </section>
-                    {/* end hero */}
 
-                    {/* popular crypto */}
                     <section className="popular-crypto position-relative pb-5">
                         <img src={MoneroIcon} className="image-coin popular monero" alt="" />
                         <div className="container index-2">
@@ -285,7 +293,7 @@ class Landing extends React.Component<Props> {
                                 Most popular coins to trade
                             </p>
                             <div className="market-list">
-                                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                {/* <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                                     <li className="nav-item">
                                         <a
                                             className="nav-link active"
@@ -322,7 +330,7 @@ class Landing extends React.Component<Props> {
                                             Top Gainers
                                         </a>
                                     </li>
-                                </ul>
+                                </ul> */}
 
                                 <div>
                                     <MarketsTable />
@@ -330,7 +338,212 @@ class Landing extends React.Component<Props> {
                             </div>
                         </div>
                     </section>
-                    {/* end popular crypto */}
+
+                    <section className="feature-section position-relative py-5">
+                        <img src={LiteIcon} className="image-coin feature lite" alt="" />
+                        <img src={BitcoinIcon} className="image-coin feature btc" alt="" />
+                        <div className="container index-2">
+                            <h2 className="text-title-1 white-text text-center font-extrabold ">Attractive Features</h2>
+                            <p className=" mb-24 text-md font-normal grey-text-accent text-center mb-36">
+                                Windaful makes playing the UK's best raffles easy and fun.
+                            </p>
+                            <div className="row">
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon1 />
+                                        <p className="mb-0 ml-2 text-ms white-text">Weekly Trading Contest</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon2 />
+                                        <p className="mb-0 ml-2 text-ms white-text">Less Commission On Trade</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon3 />
+                                        <p className="mb-0 ml-2 text-ms white-text">Safe And Secure Trading platform</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon4 />
+                                        <p className="mb-0 ml-2 text-ms white-text">Instant Withdraw Process</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon5 />
+                                        <p className="mb-0 ml-2 text-ms white-text">Monthly Commition</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                                <div className="col-lg-4 col-sm-6 mb-24">
+                                    <div className="d-flex align-items-center mb-3">
+                                        <FeatureIcon6 />
+                                        <p className="mb-0 ml-2 text-ms white-text">24/7 Support Team</p>
+                                    </div>
+                                    <p className="mb-0 text-ms grey-text-accent">
+                                        Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="aplication-section py-5">
+                        <div className="container">
+                            <div className="row align-items-center">
+                                <div className="col-lg-6">
+                                    <img src={LandingAplicationImage} className="w-100" alt="" />
+                                </div>
+                                <div className="col-lg-6">
+                                    <h4 className="mb-0 text-title-2 text-white font-extrabold">
+                                        Trade Anytime, Anywhere
+                                    </h4>
+                                    <p className="text-ms grey-text-accent font-normal mb-24">
+                                        Compatible with multiple devices, start trading with safety and convenience.
+                                    </p>
+                                    <div className="row">
+                                        <div className="col-4">
+                                            <div className="mr-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+                                                <AndroidIcon className="icon-aplication" />
+                                                <p className="text-ms white-text mt-2 mb-0">Android APK</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-4">
+                                            <div className="mr-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+                                                <AppleStoreIcon className="icon-aplication" />
+                                                <p className="text-ms white-text mt-2 mb-0">App Store</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-4">
+                                            <div className="mr-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+                                                <GooglePlayIcon className="icon-aplication" />
+                                                <p className="text-ms white-text mt-2 mb-0">Google Play</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-4">
+                                            <div className="mr-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+                                                <WindowsIcon className="icon-aplication" />
+                                                <p className="text-ms white-text mt-2 mb-0">Windows</p>
+                                            </div>
+                                        </div>
+                                        <div className="col-4">
+                                            <div className="mr-3 mb-3 d-flex flex-column justify-content-center align-items-center">
+                                                <MacOsIcon className="icon-aplication" />
+                                                <p className="text-ms white-text mt-2 mb-0">Mac Os</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section className="analysis-section position-relative py-5">
+                        <img src={EtherumIcon} className="image-coin analysis eth" alt="" />
+                        <img src={BitcoinIcon} className="image-coin analysis btc" alt="" />
+                        <img src={MoneroIcon} className="image-coin analysis monero" alt="" />
+                        <div className="container index-2">
+                            <h2 className="text-title-1 white-text text-center font-extrabold ">
+                                Crypto Market Analysis
+                            </h2>
+                            <p className=" mb-24 text-md font-normal grey-text-accent text-center mb-36">
+                                Tincidunt id nibh orci nibh justo. Purus et turpis nulla elementum, sed vel.
+                            </p>
+                            <div className="row mt-5">
+                                <div className="col-lg-6 mb-4">
+                                    <div className="d-flex">
+                                        <img src={MarketAnalysisImage} className="image-analysis" alt="image" />
+                                        <div className="ml-4">
+                                            <p className="mb-8 text-lg white-text font-bold">Gain the best exchange</p>
+                                            <p className="mb-36 text-sm grey-text-accent">
+                                                Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare
+                                                risus.
+                                            </p>
+                                            <a
+                                                href="./Screen/Announcement/index.html"
+                                                className="gradient-text text-ms ">
+                                                Learn more
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 mb-4">
+                                    <div className="d-flex">
+                                        <img src={MarketAnalysisImage} className="image-analysis" alt="image" />
+                                        <div className="ml-4">
+                                            <p className="mb-8 text-lg white-text font-bold">Gain the best exchange</p>
+                                            <p className="mb-36 text-sm grey-text-accent">
+                                                Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare
+                                                risus.
+                                            </p>
+                                            <a
+                                                href="./Screen/Announcement/index.html"
+                                                className="gradient-text text-ms ">
+                                                Learn more
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 mb-4">
+                                    <div className="d-flex">
+                                        <img src={MarketAnalysisImage} className="image-analysis" alt="image" />
+                                        <div className="ml-4">
+                                            <p className="mb-8 text-lg white-text font-bold">Gain the best exchange</p>
+                                            <p className="mb-36 text-sm grey-text-accent">
+                                                Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare
+                                                risus.
+                                            </p>
+                                            <a
+                                                href="./Screen/Announcement/index.html"
+                                                className="gradient-text text-ms ">
+                                                Learn more
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 mb-4">
+                                    <div className="d-flex">
+                                        <img src={MarketAnalysisImage} className="image-analysis" alt="image" />
+                                        <div className="ml-4">
+                                            <p className="mb-8 text-lg white-text font-bold">Gain the best exchange</p>
+                                            <p className="mb-36 text-sm grey-text-accent">
+                                                Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare
+                                                risus.
+                                            </p>
+                                            <a
+                                                href="./Screen/Announcement/index.html"
+                                                className="gradient-text text-ms ">
+                                                Learn more
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-center mt-3">
+                                <a href="" className="btn btn-primary lg">
+                                    Show More
+                                </a>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         );
