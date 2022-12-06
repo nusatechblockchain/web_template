6c2c82cd9f52de19a5566f158be67d89ed962664
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-06 15:15:41 +0700

    create market popular

--- src/desktop/containers/WalletsOverview/index.tsx
+++ src/desktop/containers/WalletsOverview/index.tsx
@@ -38,7 +38,9 @@ const WalletsOverview: FC<Props> = (props: Props): ReactElement => {
     const { formatMessage } = useIntl();
     const { isP2PEnabled } = props;
     const history = useHistory();
-    const translate = useCallback((id: string, value?: any) => formatMessage({ id: id }, { ...value }), [formatMessage]);
+    const translate = useCallback((id: string, value?: any) => formatMessage({ id: id }, { ...value }), [
+        formatMessage,
+    ]);
     const wallets = useSelector(selectWallets);
     const abilities = useSelector(selectAbilities);
     const currencies = useSelector(selectCurrencies);
@@ -52,11 +54,11 @@ const WalletsOverview: FC<Props> = (props: Props): ReactElement => {
 
     useEffect(() => {
         if (wallets.length && currencies.length) {
-            const extendedWallets: ExtendedWallet[] = currencies.map(cur => {
+            const extendedWallets: ExtendedWallet[] = currencies.map((cur) => {
                 if (cur.status === 'hidden' && user.role !== 'admin' && user.role !== 'superadmin') {
                     return null;
                 }
-                const spotWallet = wallets.find(i => i.currency === cur.id);
+                const spotWallet = wallets.find((i) => i.currency === cur.id);
                 return {
                     ...spotWallet,
                     spotBalance: spotWallet ? spotWallet.balance : '0',
@@ -64,72 +66,89 @@ const WalletsOverview: FC<Props> = (props: Props): ReactElement => {
                 };
             });
 
-            const extendedWalletsFilter = extendedWallets.filter(item => item && item.currency);
+            const extendedWalletsFilter = extendedWallets.filter((item) => item && item.currency);
             setFilteredWallets(extendedWalletsFilter);
         }
     }, [wallets, currencies, isP2PEnabled]);
 
-    const headerTitles = useCallback(() => ([
-        'Code',
-        translate('page.body.wallets.overview.header.wallet'),
-        translate('page.body.wallets.overview.header.total'),
-        translate('page.body.wallets.overview.header.estimated'),
-        'Locked Balance',
-        '',
-    ]), [isP2PEnabled]);
+    const headerTitles = useCallback(
+        () => [
+            'Code',
+            translate('page.body.wallets.overview.header.wallet'),
+            translate('page.body.wallets.overview.header.total'),
+            translate('page.body.wallets.overview.header.estimated'),
+            'Locked Balance',
+            '',
+        ],
+        [isP2PEnabled]
+    );
 
-    const handleClickDeposit = useCallback(currency => {
-        history.push(`/wallets/${currency}/deposit`);
-    }, [history]);
+    const handleClickDeposit = useCallback(
+        (currency) => {
+            history.push(`/wallets/${currency}/deposit`);
+        },
+        [history]
+    );
 
-    const handleClickWithdraw = useCallback(currency => {
-        history.push(`/wallets/${currency}/withdraw`);
-    }, [history]);
-
+    const handleClickWithdraw = useCallback(
+        (currency) => {
+            history.push(`/wallets/${currency}/withdraw`);
+        },
+        [history]
+    );
 
     const retrieveData = React.useCallback(() => {
-        const list = nonZeroSelected ? filteredWallets.filter(i => i.balance && Number(i.balance) > 0) : filteredWallets;
-        const filteredList = list.filter(i => !filterValue || i.name?.toLocaleLowerCase().includes(filterValue.toLowerCase()) || i.currency?.toLocaleLowerCase().includes(filterValue.toLowerCase()));
+        const list = nonZeroSelected
+            ? filteredWallets.filter((i) => i.balance && Number(i.balance) > 0)
+            : filteredWallets;
+        const filteredList = list.filter(
+            (i) =>
+                !filterValue ||
+                i.name?.toLocaleLowerCase().includes(filterValue.toLowerCase()) ||
+                i.currency?.toLocaleLowerCase().includes(filterValue.toLowerCase())
+        );
 
-        return !filteredList.length ? [[]] : filteredList.map((item, index) => {
-            const {
-                currency,
-                iconUrl,
-                name,
-                fixed,
-                spotBalance,
-                spotLocked,
-            } = item;
-            const totalBalance = Number(spotBalance) + Number(spotLocked);
-            const estimatedValue = Number(totalBalance) && currency ? estimateUnitValue(currency.toUpperCase(), VALUATION_PRIMARY_CURRENCY, +totalBalance, currencies, markets, tickers) : Decimal.format(0, fixed);
+        return !filteredList.length
+            ? [[]]
+            : filteredList.map((item, index) => {
+                  const { currency, iconUrl, name, fixed, spotBalance, spotLocked } = item;
+                  const totalBalance = Number(spotBalance) + Number(spotLocked);
+                  const estimatedValue =
+                      Number(totalBalance) && currency
+                          ? estimateUnitValue(
+                                currency.toUpperCase(),
+                                VALUATION_PRIMARY_CURRENCY,
+                                +totalBalance,
+                                currencies,
+                                markets,
+                                tickers
+                            )
+                          : Decimal.format(0, fixed);
 
-            return [
-                <div>{currency.toUpperCase()}</div>,
-                <div key={index}>
-                    <img alt={currency?.toUpperCase()} src={iconUrl} style={{height:'24px'}}/>
-                    {name}
-                </div>,
-                <Decimal key={index} fixed={fixed} thousSep=",">{totalBalance ? totalBalance.toString() : '0'}</Decimal>,
-                formatWithSeparators(estimatedValue, ','),
-                <Decimal key={index} fixed={fixed} thousSep=",">{spotLocked}</Decimal>,
-                <div key={index}>
-                    <Button onClick={() => handleClickDeposit(currency)} variant="primary">
-                        {translate('page.body.wallets.overview.action.deposit')}
-                    </Button>
-                    <Button onClick={() => handleClickWithdraw(currency)} variant="danger">
-                        {translate('page.body.wallets.overview.action.withdraw')}
-                    </Button>
-                </div>,
-            ];
-        })
-    }, [
-        filteredWallets,
-        nonZeroSelected,
-        abilities,
-        currencies,
-        markets,
-        tickers,
-    ]);
+                  return [
+                      <div>{currency.toUpperCase()}</div>,
+                      <div key={index}>
+                          <img alt={currency?.toUpperCase()} src={iconUrl} style={{ height: '24px' }} />
+                          {name}
+                      </div>,
+                      <Decimal key={index} fixed={fixed} thousSep=",">
+                          {totalBalance ? totalBalance.toString() : '0'}
+                      </Decimal>,
+                      formatWithSeparators(estimatedValue, ','),
+                      <Decimal key={index} fixed={fixed} thousSep=",">
+                          {spotLocked}
+                      </Decimal>,
+                      <div key={index}>
+                          <Button onClick={() => handleClickDeposit(currency)} variant="primary">
+                              {translate('page.body.wallets.overview.action.deposit')}
+                          </Button>
+                          <Button onClick={() => handleClickWithdraw(currency)} variant="danger">
+                              {translate('page.body.wallets.overview.action.withdraw')}
+                          </Button>
+                      </div>,
+                  ];
+              });
+    }, [filteredWallets, nonZeroSelected, abilities, currencies, markets, tickers]);
 
     return (
         <div>
@@ -142,11 +161,9 @@ const WalletsOverview: FC<Props> = (props: Props): ReactElement => {
                 handleClickCheckBox={setNonZeroSelected}
             />
             <h3>Wallet Data</h3>
-            <Table header={headerTitles()} data={retrieveData()}/>
+            <Table header={headerTitles()} data={retrieveData()} />
         </div>
     );
 };
 
-export {
-    WalletsOverview,
-};
+export { WalletsOverview };
