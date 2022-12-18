b1f046115ddce30340cc5bf06493ee136cd2e0e3
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-18 21:26:35 +0700

    create market list

--- src/mobile/screens/MarketList/index.tsx
+++ src/mobile/screens/MarketList/index.tsx
@@ -4,25 +4,80 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import { Table } from '../../../components';
 import { BtcIcon } from '../../../assets/images/CoinIcon';
+import { Link } from 'react-router-dom';
 
 const MarketListlMobileScreen: React.FC = () => {
     const [key, setKey] = React.useState('tranding');
+    const [showSearch, setShowSearch] = React.useState(false);
 
-    const MarketFavorite = [{ icon_url: 'Name', name: 'BTC', id: 'btc', price_change_percent: '2.00%' }];
+    const MarketFavorite = [
+        {
+            icon_url: <BtcIcon />,
+            name: 'BTC',
+            id: 'btc',
+            volume: '18.122.212',
+            last_price: '323.903.231',
+            price_change_percent: '+2.00%',
+        },
+        {
+            icon_url: <BtcIcon />,
+            name: 'BTC',
+            id: 'btc',
+            volume: '18.122.212',
+            last_price: '323.903.231',
+            price_change_percent: '+2.00%',
+        },
+        {
+            icon_url: <BtcIcon />,
+            name: 'BTC',
+            id: 'btc',
+            volume: '18.122.212',
+            last_price: '323.903.231',
+            price_change_percent: '-2.00%',
+        },
+        {
+            icon_url: <BtcIcon />,
+            name: 'BTC',
+            id: 'btc',
+            volume: '18.122.212',
+            last_price: '323.903.231',
+            price_change_percent: '-2.00%',
+        },
+    ];
+    const renderTableHeader = [
+        <p className="mb-0 text-sm grey-text">Name</p>,
+        <p className="mb-0 text-sm grey-text">Volume</p>,
+        <p className="mb-0 text-sm grey-text">Change</p>,
+        '',
+    ];
 
     const renderDataTable = (data) => {
         return data.map((item) => [
             <div className="d-flex align-items-center text-sm">
-                <img src={item && item.currency && item.currency.icon_url} alt="coin" className="small-coin-icon" />
-                <p className="mb-0 white-text text-sm ml-2">{item && item.currency && item.currency.name}</p>
-                <p className="mb-0 grey-text text-xs ml-2">
-                    {item && item.currency && item.currency.id && item.currency.id.toUpperCase()}
-                </p>
+                {item && item.icon_url}
+                <div className="ml-1">
+                    <p className="mb-0 grey-text-accent mb-0 text-sm ml-2">
+                        {item && item.name} /{' '}
+                        <span className="text-xxs grey-text">{item && item.id.toUpperCase()} </span>
+                    </p>
+                    <p className="mb-0 grey-text text-xxs ml-2">vol : {item && item.volume}</p>
+                </div>
             </div>,
-            <div></div>,
-            <p className={`badge white-text font-bold ${item.change.includes('-') ? 'badge-danger' : 'badge-success'}`}>
+            <p
+                className={`grey-text text-sm font-bold mb-0 ${
+                    item.price_change_percent.includes('-') ? 'danger-text' : 'contrast-text'
+                }`}>
+                {item.last_price}
+            </p>,
+            <p
+                className={`badge white-text font-bold mb-0 ${
+                    item.price_change_percent.includes('-') ? 'badge-danger' : 'badge-success'
+                }`}>
                 {item && item.price_change_percent}
             </p>,
+            <Link to={`/trading/${item.id}`} className="gradient-text text-sm">
+                Trade
+            </Link>,
         ]);
     };
     return (
@@ -34,44 +89,61 @@ const MarketListlMobileScreen: React.FC = () => {
                         <div className="mr-2 d-inline">
                             <EditIcon />
                         </div>
-                        <SearchIcon />
+                        <div className="d-inline" onClick={() => setShowSearch(true)}>
+                            <SearchIcon />
+                        </div>
                     </div>
                 </nav>
-                <div id="form-search" className="mb-24">
-                    <form>
-                        <div className="d-flex justify-content-between align-items-center form-wrapper">
-                            <div className="input-group">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <SearchIcon />
-                                    </span>
+                {showSearch && (
+                    <div className="mb-24 mt-24">
+                        <form>
+                            <div className="d-flex justify-content-between align-items-center form-wrapper">
+                                <div className="input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            <SearchIcon />
+                                        </span>
+                                    </div>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Enter the currency you are looking for."
+                                        aria-label="search"
+                                        aria-describedby="basic-addon1"
+                                    />
                                 </div>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Enter the currency you are looking for."
-                                    aria-label="search"
-                                    aria-describedby="basic-addon1"
-                                />
+                                <button id="cancel" className="btn-cancel" type="button">
+                                    Cancel
+                                </button>
                             </div>
-                            <button id="cancel" className="btn-cancel" type="button">
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="mb-3">
+                        </form>
+                    </div>
+                )}
+                <Tabs id="controlled-tab-example" activeKey={key} onSelect={(k) => setKey(k)} className="">
                     <Tab eventKey="tranding" title="Tranding">
-                        <Table data={renderDataTable(MarketFavorite)} />
+                        <div className="table-mobile-wrapper">
+                            <Table data={renderDataTable(MarketFavorite)} header={renderTableHeader} />
+                        </div>
                     </Tab>
-                    <Tab eventKey="new-volume" title="New Volume">
-                        <Table data={renderDataTable(MarketFavorite)} />
+                    <Tab eventKey="all-crypto" title="All-crypto">
+                        <div className="table-mobile-wrapper">
+                            <Table data={renderDataTable(MarketFavorite)} header={renderTableHeader} />
+                        </div>
                     </Tab>
-                    <Tab eventKey="gainers" title="Gainers">
-                        <Table data={renderDataTable(MarketFavorite)} />
+                    <Tab eventKey="spot" title="Spot">
+                        <div className="table-mobile-wrapper">
+                            <Table data={renderDataTable(MarketFavorite)} header={renderTableHeader} />
+                        </div>
                     </Tab>
-                    <Tab eventKey="loser" title="Loser">
-                        <div className="table-mobile-wrapper">{/* <Table data={renderDataTable(dataLosers)} /> */}</div>
+                    <Tab eventKey="future" title="Future">
+                        <div className="table-mobile-wrapper">
+                            <Table data={renderDataTable(MarketFavorite)} header={renderTableHeader} />
+                        </div>
+                    </Tab>
+                    <Tab eventKey="New Listing" title="New Listing">
+                        <div className="table-mobile-wrapper">
+                            <Table data={renderDataTable(MarketFavorite)} header={renderTableHeader} />
+                        </div>
                     </Tab>
                 </Tabs>
             </div>
