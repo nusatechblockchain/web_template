041042953f9cc0e3ece664ba74d06cec0163eae4
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-18 14:01:07 +0700

    create change password screen

--- src/mobile/screens/ChangePassword/index.tsx
+++ src/mobile/screens/ChangePassword/index.tsx
@@ -70,8 +70,6 @@ const ChangePasswordMobileScreen: React.FC = () => {
         }
     };
 
-    console.log(isValidForm);
-
     React.useEffect(() => {
         if (passwordValue == confirmPasswordValue) {
             setPasswordMatch(true);
@@ -85,7 +83,7 @@ const ChangePasswordMobileScreen: React.FC = () => {
     };
     return (
         <React.Fragment>
-            <div className="mobile-container change-password home-screen dark-bg-main">
+            <div className="mobile-container change-password no-header home-screen dark-bg-main">
                 <div className="head-container position-relative">
                     <Link to={''} className="cursor-pointer position-absolute">
                         <ArrowLeft className={'back'} />
@@ -93,74 +91,78 @@ const ChangePasswordMobileScreen: React.FC = () => {
                     <h1 className="text-center text-md grey-text-accent font-bold">Profile Setting</h1>
                 </div>
                 <div className="divider" />
-                <form className="form-container">
+                <form>
                     <p className="text-sm grey-text">
                         Change your password and by entering the code that has been sent previously
                     </p>
-                    <CustomInput
-                        defaultLabel="Old Password"
-                        inputValue={oldPasswordValue}
-                        label="Old Password"
-                        placeholder="Old password"
-                        type="password"
-                        classNameLabel="white-text text-sm"
-                        classNameInput="text-ms input-mobile"
-                        handleChangeInput={(e) => setOldPasswordvalue(e)}
-                        labelVisible
-                    />
-                    <CustomInput
-                        defaultLabel="Set New Password "
-                        inputValue={passwordValue}
-                        label="Set New Password"
-                        placeholder="Set New Password"
-                        type="password"
-                        labelVisible
-                        classNameLabel="white-text text-sm"
-                        classNameInput={`input-mobile ${
-                            passwordNewFocus &&
-                            (!passwordErrorFirstSolved || !passwordErrorSecondSolved || !passwordErrorThirdSolved) &&
-                            'error'
-                        }`}
-                        autoFocus={false}
-                        handleFocusInput={handleFocusNewPassword}
-                        handleChangeInput={handleChangeNewPassword}
-                    />
-
-                    {passwordNewFocus &&
-                        (!passwordErrorFirstSolved || !passwordErrorSecondSolved || !passwordErrorThirdSolved) && (
-                            <p className="danger-text m-0 mb-24 text-xs">Password Strength must be GOOD</p>
-                        )}
                     <div>
-                        <PasswordStrengthMeter
-                            minPasswordEntropy={passwordMinEntropy()}
-                            currentPasswordEntropy={currentPasswordEntropy}
-                            passwordExist={passwordValue !== ''}
-                            passwordErrorFirstSolved={passwordErrorFirstSolved}
-                            passwordErrorSecondSolved={passwordErrorSecondSolved}
-                            passwordErrorThirdSolved={passwordErrorThirdSolved}
-                            passwordPopUp={passwordPopUp}
-                            translate={translate}
+                        <CustomInput
+                            defaultLabel="Old Password"
+                            inputValue={oldPasswordValue}
+                            label="Old Password"
+                            placeholder="Old password"
+                            type="password"
+                            classNameLabel="white-text text-sm"
+                            classNameInput="text-ms input-mobile"
+                            handleChangeInput={(e) => setOldPasswordvalue(e)}
+                            labelVisible
                         />
+                        <CustomInput
+                            defaultLabel="Set New Password "
+                            inputValue={passwordValue}
+                            label="Set New Password"
+                            placeholder="Set New Password"
+                            type="password"
+                            labelVisible
+                            classNameLabel="white-text text-sm"
+                            classNameInput={`input-mobile ${
+                                passwordNewFocus &&
+                                (!passwordErrorFirstSolved ||
+                                    !passwordErrorSecondSolved ||
+                                    !passwordErrorThirdSolved) &&
+                                'error'
+                            }`}
+                            autoFocus={false}
+                            handleFocusInput={handleFocusNewPassword}
+                            handleChangeInput={handleChangeNewPassword}
+                        />
+
+                        {passwordNewFocus &&
+                            (!passwordErrorFirstSolved || !passwordErrorSecondSolved || !passwordErrorThirdSolved) && (
+                                <p className="danger-text m-0 mb-24 text-xs">Password Strength must be GOOD</p>
+                            )}
+                        <div>
+                            <PasswordStrengthMeter
+                                minPasswordEntropy={passwordMinEntropy()}
+                                currentPasswordEntropy={currentPasswordEntropy}
+                                passwordExist={passwordValue !== ''}
+                                passwordErrorFirstSolved={passwordErrorFirstSolved}
+                                passwordErrorSecondSolved={passwordErrorSecondSolved}
+                                passwordErrorThirdSolved={passwordErrorThirdSolved}
+                                passwordPopUp={passwordPopUp}
+                                translate={translate}
+                            />
+                        </div>
+                        <CustomInput
+                            defaultLabel="Confirm New Password"
+                            inputValue={confirmPasswordValue}
+                            label="Confirm New Password"
+                            placeholder="Confirm New password"
+                            type="password"
+                            classNameLabel="white-text text-sm"
+                            classNameInput="text-ms input-mobile"
+                            handleChangeInput={(e) => setConfirmPasswordvalue(e)}
+                            labelVisible
+                        />
+                        {!passwordMatch && <p className="danger-text font-normal text-xs">Password doesn't match</p>}
+                        <button
+                            type="button"
+                            className="btn-primary btn-block btn-mobile"
+                            onClick={handleChangePassword}
+                            disabled={isValidForm()}>
+                            Submit
+                        </button>
                     </div>
-                    <CustomInput
-                        defaultLabel="Confirm New Password"
-                        inputValue={confirmPasswordValue}
-                        label="Confirm New Password"
-                        placeholder="Confirm New password"
-                        type="password"
-                        classNameLabel="white-text text-sm"
-                        classNameInput="text-ms input-mobile"
-                        handleChangeInput={(e) => setConfirmPasswordvalue(e)}
-                        labelVisible
-                    />
-                    {!passwordMatch && <p className="danger-text font-normal text-xs">Password doesn't match</p>}
-                    <button
-                        type="button"
-                        className="btn-primary btn-block btn-mobile"
-                        onClick={handleChangePassword}
-                        disabled={isValidForm()}>
-                        Submit
-                    </button>
                 </form>
             </div>
         </React.Fragment>
