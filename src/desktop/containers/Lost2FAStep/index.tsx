4bcdd60af22262fc0113a082a4eb52c98853bf51
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-24 09:57:21 +0700

    lost to fa page:

--- src/desktop/containers/Lost2FAStep/index.tsx
+++ src/desktop/containers/Lost2FAStep/index.tsx
@@ -1,106 +1,22 @@
 import React from 'react';
-import { useIntl } from 'react-intl';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
 import './Lost2FAStep.pcss';
-import { InputFile } from '../../components';
 import lostImg from '../../../../public/img/lost-two-fa.svg';
 
-interface StepElement {
-    stepOne: boolean;
-    stepTwo: boolean;
-    stepThree: boolean;
-    handleNextStep;
-    handleSubmit;
-}
-
-type Props = StepElement;
-
-export const Lost2FAStep: React.FC<Props> = (props: Props): React.ReactElement => {
-    const { stepOne, stepTwo, stepThree, handleNextStep, handleSubmit } = props;
-
+export const Lost2FAStep: React.FC = (): React.ReactElement => {
     return (
         <React.Fragment>
-            {stepOne && (
-                <div className="tabs-item" id="tabs-one">
-                    <p className="grey-text text-sm">Send email verification to get the code</p>
-                    <div className="form">
-                        <InputFile />
-                        <div className="d-flex justify-content-end">
-                            <button
-                                onClick={handleNextStep}
-                                type="button"
-                                className="btn btn-primary w-150"
-                                id="tabs-one-button">
-                                Next
-                            </button>
-                        </div>
+            <div className="tabs-item" id="tabs-three">
+                <div className="__three  show">
+                    <div className="d-flex justify-content-center mb-24">
+                        <img src={lostImg} alt="lost two fa icon" />
                     </div>
+                    <h3 className="white-text text-center mb-24">Please Contact Support Admin</h3>
+                    <p className=" grey-text-accent text-center">
+                        Please contact the heaven exchange admin contact and report that you have lost the two fa code
+                        then you will be assisted to get it back
+                    </p>
                 </div>
-            )}
-
-            {stepTwo && (
-                <div className="tabs-item" id="tabs-two">
-                    <p className="grey-text text-sm">Upload multiple documents for identity verification purposes</p>
-                    <div className="form">
-                        <div className="group mb-12">
-                            <label htmlFor="custom-file" className="text-ms white-text mb-1">
-                                Front ID <span className="danger-text">*</span>
-                            </label>
-                            <InputFile />
-                            <label htmlFor="custom-file" className="text-xs grey-text">
-                                Supported formats : JPG and PNG. Max file size 4MB
-                            </label>
-                        </div>
-                        <div className="group mb-12">
-                            <label htmlFor="custom-file" className="text-ms white-text mb-1">
-                                Back ID <span className="danger-text">*</span>
-                            </label>
-                            <InputFile />
-                            <label htmlFor="custom-file" className="text-xs grey-text">
-                                Supported formats : JPG and PNG. Max file size 4MB
-                            </label>
-                        </div>
-                        <div className="group mb-12">
-                            <label htmlFor="custom-file" className="text-ms white-text mb-1">
-                                Upload Selfie With Identitity Document <span className="danger-text">*</span>
-                            </label>
-                            <InputFile />
-                            <label htmlFor="custom-file" className="text-xs grey-text">
-                                Upload Selfie With Identitity Document *
-                            </label>
-                        </div>
-                        <div className="d-flex justify-content-end">
-                            <button
-                                onClick={handleSubmit}
-                                type="button"
-                                className="btn btn-primary w-150"
-                                id="tabs-two-button">
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            )}
-
-            {stepThree && (
-                <div className="tabs-item" id="tabs-three">
-                    <div className="__three  show">
-                        <div className="d-flex justify-content-center mb-24">
-                            <img src={lostImg} alt="lost two fa icon" />
-                        </div>
-                        <h3 className="white-text text-center mb-24">Waiting for Review</h3>
-                        <p className=" grey-text-accent text-center">
-                            We will complete the verification within 1 to 3 business days and notify the result to you
-                            through SMS and email.
-                        </p>
-                        <p className=" grey-text-accent text-center mb-24">
-                            To ensure your account security, the withdrawal services on KuCoin and Futures will be
-                            temporarily closed for 24 hours after the setting is approved.
-                        </p>
-                    </div>
-                </div>
-            )}
+            </div>
         </React.Fragment>
     );
 };
--- src/desktop/screens/Lost2FAScreen/index.tsx
+++ src/desktop/screens/Lost2FAScreen/index.tsx
@@ -1,15 +1,12 @@
 import cx from 'classnames';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useIntl } from 'react-intl';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router';
-import { useReduxSelector } from '../../../hooks';
-import { selectMobileDeviceState } from '../../../modules';
 import './Lost2FAScreen.pcss';
 import bgImage from '../../../../public/img/background.png';
 import { ArrowLeftGradient } from 'src/assets/images/ArrowLeftIcon';
-import { InputFile } from '../../components';
-import { Lost2FAInfo, Lost2FATimeline, Lost2FAStep } from '../../containers';
+import { Lost2FAStep } from '../../containers';
 
 export const Lost2FAScreen: React.FC = () => {
     const dispatch = useDispatch();
@@ -53,20 +50,8 @@ export const Lost2FAScreen: React.FC = () => {
                                 </div>
                             </div>
                             <div className="row justify-content-center">
-                                <div className="col-8 dark-bg-main py-5 px-5">
-                                    {!stepThree && <Lost2FAInfo />}
-                                    <Lost2FATimeline
-                                        stepOneActive={stepOneActive}
-                                        stepTwoActive={stepTwoActive}
-                                        stepThreeActive={stepThreeActive}
-                                    />
-                                    <Lost2FAStep
-                                        stepOne={stepOne}
-                                        stepTwo={stepTwo}
-                                        stepThree={stepThree}
-                                        handleNextStep={handleNextStep}
-                                        handleSubmit={handleSubmit}
-                                    />
+                                <div className="col-12 col-md-10 dark-bg-main py-5 px-5">
+                                    <Lost2FAStep />
                                 </div>
                             </div>
                         </div>
