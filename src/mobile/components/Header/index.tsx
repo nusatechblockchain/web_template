ffaeb00e754df548300238b623438f69d73fc6a7
hafidzalafaf <hafidzalafaf@gmail.com>
2022-12-28 12:04:32 +0700

    create header component

--- src/mobile/components/Header/index.tsx
+++ src/mobile/components/Header/index.tsx
@@ -1,6 +1,176 @@
 import * as React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { selectUserLoggedIn, selectUserInfo, logoutFetch } from '../../../modules';
+import { LogoIcon } from '../../assets/Logo';
+import { ScanIcon } from '../../assets/ScanIcon';
+import { SearchIcon } from '../../assets/SearchIcon';
+import { UserIcon } from '../../assets/UserIcon';
+import { ArrowLeft, ArrowRight } from '../../assets/Arrow';
+import { CopyableTextField } from '../../../components';
+import Avatar from '/public/img-mobile/avatar.png';
+import {
+    Announcement,
+    ApiManagement,
+    Dashboard,
+    Faq,
+    MarketOrder,
+    Referral,
+    Security,
+    TradeHistory,
+    Wallet,
+} from '../../assets/Sidebar';
+import { CalendarIcon } from '../../assets/CalendarIcon';
 const HeaderComponent: React.FC = () => {
-    return <div></div>;
+    const [showSidebar, setShowSidebar] = React.useState(false);
+    const userLoggedIn = useSelector(selectUserLoggedIn);
+    const user = useSelector(selectUserInfo);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const handleLogout = async () => {
+        await dispatch(logoutFetch());
+        history.push('/trading');
+    };
+
+    const sidebarMenu = [
+        { icon: <Dashboard />, name: 'Dashborad', path: '/profile', showNotLoggedin: false },
+        { icon: <Wallet />, name: 'Wallet', path: '/wallets', showNotLoggedin: false },
+        { icon: <MarketOrder />, name: 'MarketOrder', path: '/market-order', showNotLoggedin: false },
+        { icon: <TradeHistory />, name: 'Trade History', path: '/history-trade', showNotLoggedin: false },
+        {
+            icon: <CalendarIcon />,
+            name: 'Transaction History',
+            path: '/history-transaction',
+            showNotLoggedin: false,
+        },
+        { icon: <Security />, name: 'Security', path: '/security', showNotLoggedin: false },
+        { icon: <Referral />, name: 'Referral', path: '/referral', showNotLoggedin: false },
+        { icon: <ApiManagement />, name: 'Api Management', path: '/api-key', showNotLoggedin: false },
+        { icon: <Announcement />, name: 'Announcement', path: '/announcement', showNotLoggedin: true },
+        { icon: <Faq />, name: 'Faq', path: '/faq', showNotLoggedin: true },
+    ];
+    return (
+        <div>
+            <div className="px-24 dark-bg-main">
+                <nav className="navbar-mobile fixed-top px-24 py-3 dark-bg-main">
+                    <a className="navbar-brand">
+                        <LogoIcon className={''} />
+                    </a>
+                    <div className="d-flex align-items-center">
+                        <ScanIcon className={'mr-2'} />
+                        <SearchIcon className={'mr-2'} />
+                        <div className="cursor-pointer" onClick={() => setShowSidebar(true)}>
+                            <UserIcon className={'cursor-pointer'} />
+                        </div>
+                    </div>
+                </nav>
+                <div id="sidebar" className={`sidebar-offcanvas dark-bg-accent ${showSidebar ? 'show' : 'hide'}`}>
+                    <div className="sidebar d-flex justify-content-between align-items-start w-100 pb-5">
+                        <div className="sidebar-container w-100">
+                            <div className="sidebar-head mb-24 px-24" onClick={() => setShowSidebar(false)}>
+                                <ArrowLeft className={'cursorPointer'} />
+                            </div>
+                            {userLoggedIn ? (
+                                <Link to={'/profile'} onClick={() => setShowSidebar(false)}>
+                                    <div className="card-user-info d-flex align-items-center px-24 mb-24">
+                                        <img src={Avatar} className="avatar-image" alt="ava" />
+                                        <div className="user-info d-flex justify-content-between align-items-center ml-2 w-100">
+                                            <div>
+                                                <div className="user-name d-flex align-items-center">
+                                                    <h1 className="gradient-text text-md font-bold mb-2">
+                                                        Hi,{' '}
+                                                        {user && user.username !== null
+                                                            ? user.username
+                                                            : 'The Awesome Member'}
+                                                    </h1>
+                                                    <p className="badge badge-warning white-text mb-0 ml-3">
+                                                        Unverifed
+                                                    </p>
+                                                </div>
+                                                <div className="user-id d-flex align-items-center">
+                                                    <h3 className="text-sm grey-text d-flex align-items-center">
+                                                        UID :{' '}
+                                                        <CopyableTextField
+                                                            value={user && user.uid}
+                                                            className="ml-3"
+                                                            fieldId="referral-code"
+                                                        />
+                                                    </h3>
+                                                </div>
+                                            </div>
+                                            <ArrowRight className={'cursor-pointer'} />
+                                        </div>
+                                    </div>
+                                </Link>
+                            ) : (
+                                <Link to={'/signin'} onClick={() => setShowSidebar(false)}>
+                                    <div className="card-user-info d-flex align-items-center px-24 mb-24">
+                                        <div className="user-info d-flex justify-content-between align-items-center w-100">
+                                            <div className="login-user d-flex flex-column">
+                                                <h1 className="gradient-text text-md font-bold">Login</h1>
+                                                <h3 className="grey-text text-sm font-normal">
+                                                    Welcome to Heaven Exchange
+                                                </h3>
+                                            </div>
+                                            <ArrowRight className={'cursor-pointer'} />
+                                        </div>
+                                    </div>
+                                </Link>
+                            )}
+
+                            {sidebarMenu &&
+                                sidebarMenu.map((item, key) => (
+                                    <React.Fragment>
+                                        {userLoggedIn ? (
+                                            <React.Fragment>
+                                                {key % 3 == 0 && <div className="divider" />}
+                                                <Link to={item.path} key={key} onClick={() => setShowSidebar(false)}>
+                                                    <div className=" w-100 px-24 py-3 d-flex justify-content-between align-items-center cursor-ointer">
+                                                        <div className="card-menu-name d-flex align-items-center">
+                                                            {item.icon}
+                                                            <h3 className="text-sm grey-text font-bold mb-0 ml-2">
+                                                                {item.name}
+                                                            </h3>
+                                                        </div>
+                                                        <ArrowRight className={''} />
+                                                    </div>
+                                                </Link>
+                                            </React.Fragment>
+                                        ) : item.showNotLoggedin ? (
+                                            <Link to={item.path} key={key} onClick={() => setShowSidebar(false)}>
+                                                <div className=" w-100 px-24 py-3 d-flex justify-content-between align-items-center cursor-ointer">
+                                                    <div className="card-menu-name d-flex align-items-center">
+                                                        {item.icon}
+                                                        <h3 className="text-sm grey-text font-bold mb-0 ml-2">
+                                                            {item.name}
+                                                        </h3>
+                                                    </div>
+                                                    <ArrowRight className={''} />
+                                                </div>
+                                            </Link>
+                                        ) : (
+                                            ''
+                                        )}
+                                    </React.Fragment>
+                                ))}
+
+                            {userLoggedIn && (
+                                <div className="px-24 mt-5">
+                                    <button
+                                        onClick={handleLogout}
+                                        type="button"
+                                        className="btn btn-primary btn-mobile btn-block btn-outline">
+                                        Logout
+                                    </button>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export const Header = React.memo(HeaderComponent);
--- src/mobile/screens/HomeScreen/index.tsx
+++ src/mobile/screens/HomeScreen/index.tsx
@@ -1,18 +1,6 @@
 import * as React from 'react';
-import { useIntl } from 'react-intl';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useHistory } from 'react-router-dom';
-import {
-    selectCurrentColorTheme,
-    selectUserLoggedIn,
-    Market,
-    selectMarkets,
-    selectMarketTickers,
-    setCurrentMarket,
-    selectCurrencies,
-    selectUserInfo,
-    logoutFetch,
-} from '../../../modules';
+import { useSelector } from 'react-redux';
+import { selectMarkets, selectMarketTickers, selectCurrencies } from '../../../modules';
 import { useMarketsFetch, useMarketsTickersFetch, useWalletsFetch, useDocumentTitle } from '../../../hooks';
 import { Decimal } from '../../../components';
 import Slider from 'react-slick';
@@ -29,26 +17,7 @@ import ImgCard from '/public/img-mobile/img-card.png';
 import { BgCardSmall } from '../../assets/BackgroundCard';
 import { Table } from '../../../components';
 import GrapUp from '/public/img-mobile/grap-up.png';
-import GrapDown from '/public/img-mobile/grapH-down.png';
-import { LogoIcon } from '../../assets/Logo';
-import { ScanIcon } from '../../assets/ScanIcon';
-import { SearchIcon } from '../../assets/SearchIcon';
-import { UserIcon } from '../../assets/UserIcon';
-import { ArrowLeft, ArrowRight } from '../../assets/Arrow';
-import { CopyableTextField } from '../../../components';
-import Avatar from '/public/img-mobile/avatar.png';
-import {
-    Announcement,
-    ApiManagement,
-    Dashboard,
-    Faq,
-    MarketOrder,
-    Referral,
-    Security,
-    TradeHistory,
-    Wallet,
-} from '../../assets/Sidebar';
-import { CalendarIcon } from '../../assets/CalendarIcon';
+import { ArrowRight } from '../../assets/Arrow';
 
 const noHeaderRoutes = ['/'];
 
@@ -68,28 +37,17 @@ const HomeMobileScreen: React.FC = () => {
     useMarketsFetch();
     useMarketsTickersFetch();
 
-    const dispatch = useDispatch();
-    const history = useHistory();
-
-    const userLoggedIn = useSelector(selectUserLoggedIn);
     const currencies = useSelector(selectCurrencies);
     const markets = useSelector(selectMarkets);
     const marketTickers = useSelector(selectMarketTickers);
-    const user = useSelector(selectUserInfo);
 
     const [key, setKey] = React.useState('tranding');
-    const [showSidebar, setShowSidebar] = React.useState(false);
 
     const shouldRenderHeader = !noHeaderRoutes.some((r) => location.pathname.includes(r));
 
     if (shouldRenderHeader) {
         return <React.Fragment />;
     }
-
-    const handleLogout = async () => {
-        await dispatch(logoutFetch());
-        history.push('/trading');
-    };
 
     const marketList = markets
         .map((market) => ({
@@ -114,24 +72,6 @@ const HomeMobileScreen: React.FC = () => {
     const dataGainers = marketList && marketList.sort((a, b) => +b.price_change_percent - +a.price_change_percent);
     const dataLosers = marketList && marketList.sort((a, b) => +a.price_change_percent - +b.price_change_percent);
     const dataVolume = marketList && marketList.sort((a, b) => +a.volume - +b.volume);
-
-    const sidebarMenu = [
-        { icon: <Dashboard />, name: 'Dashborad', path: '/profile', showNotLoggedin: false },
-        { icon: <Wallet />, name: 'Wallet', path: '/wallets', showNotLoggedin: false },
-        { icon: <MarketOrder />, name: 'MarketOrder', path: '/market-order', showNotLoggedin: false },
-        { icon: <TradeHistory />, name: 'Trade History', path: '/history-trade', showNotLoggedin: false },
-        {
-            icon: <CalendarIcon />,
-            name: 'Transaction History',
-            path: '/history-transaction',
-            showNotLoggedin: false,
-        },
-        { icon: <Security />, name: 'Security', path: '/security', showNotLoggedin: false },
-        { icon: <Referral />, name: 'Referral', path: '/referral', showNotLoggedin: false },
-        { icon: <ApiManagement />, name: 'Api Management', path: '/api-key', showNotLoggedin: false },
-        { icon: <Announcement />, name: 'Announcement', path: '/announcement', showNotLoggedin: true },
-        { icon: <Faq />, name: 'Faq', path: '/faq', showNotLoggedin: true },
-    ];
 
     const banner = [
         { background: Background1 },
@@ -190,121 +130,6 @@ const HomeMobileScreen: React.FC = () => {
     return (
         <React.Fragment>
             <div className="mobile-container home-screen dark-bg-main">
-                <nav className="navbar-mobile fixed-top px-24 py-3 dark-bg-main">
-                    <a className="navbar-brand">
-                        <LogoIcon className={''} />
-                    </a>
-                    <div className="d-flex align-items-center">
-                        <ScanIcon className={'mr-2'} />
-                        <SearchIcon className={'mr-2'} />
-                        <div className="cursor-pointer" onClick={() => setShowSidebar(true)}>
-                            <UserIcon className={'cursor-pointer'} />
-                        </div>
-                    </div>
-                </nav>
-                <div id="sidebar" className={`sidebar-offcanvas dark-bg-accent ${showSidebar ? 'show' : 'hide'}`}>
-                    <div className="sidebar d-flex justify-content-between align-items-start w-100 pb-5">
-                        <div className="sidebar-container w-100">
-                            <div className="sidebar-head mb-24 px-24" onClick={() => setShowSidebar(false)}>
-                                <ArrowLeft className={'cursorPointer'} />
-                            </div>
-                            {userLoggedIn ? (
-                                <Link to={'/profile'}>
-                                    <div className="card-user-info d-flex align-items-center px-24 mb-24">
-                                        <img src={Avatar} className="avatar-image" alt="ava" />
-                                        <div className="user-info d-flex justify-content-between align-items-center ml-2 w-100">
-                                            <div>
-                                                <div className="user-name d-flex align-items-center">
-                                                    <h1 className="gradient-text text-md font-bold mb-2">
-                                                        Hi,{' '}
-                                                        {user && user.username !== null
-                                                            ? user.username
-                                                            : 'The Awesome Member'}
-                                                    </h1>
-                                                    <p className="badge badge-warning white-text mb-0 ml-3">
-                                                        Unverifed
-                                                    </p>
-                                                </div>
-                                                <div className="user-id d-flex align-items-center">
-                                                    <h3 className="text-sm grey-text d-flex align-items-center">
-                                                        UID :{' '}
-                                                        <CopyableTextField
-                                                            value={user && user.uid}
-                                                            className="ml-3"
-                                                            fieldId="referral-code"
-                                                        />
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            <ArrowRight className={'cursor-pointer'} />
-                                        </div>
-                                    </div>
-                                </Link>
-                            ) : (
-                                <Link to={'/signin'}>
-                                    <div className="card-user-info d-flex align-items-center px-24 mb-24">
-                                        <div className="user-info d-flex justify-content-between align-items-center w-100">
-                                            <div className="login-user d-flex flex-column">
-                                                <h1 className="gradient-text text-md font-bold">Login</h1>
-                                                <h3 className="grey-text text-sm font-normal">
-                                                    Welcome to Heaven Exchange
-                                                </h3>
-                                            </div>
-                                            <ArrowRight className={'cursor-pointer'} />
-                                        </div>
-                                    </div>
-                                </Link>
-                            )}
-
-                            {sidebarMenu &&
-                                sidebarMenu.map((item, key) => (
-                                    <React.Fragment>
-                                        {userLoggedIn ? (
-                                            <React.Fragment>
-                                                {key % 3 == 0 && <div className="divider" />}
-                                                <Link to={item.path} key={key}>
-                                                    <div className=" w-100 px-24 py-3 d-flex justify-content-between align-items-center cursor-ointer">
-                                                        <div className="card-menu-name d-flex align-items-center">
-                                                            {item.icon}
-                                                            <h3 className="text-sm grey-text font-bold mb-0 ml-2">
-                                                                {item.name}
-                                                            </h3>
-                                                        </div>
-                                                        <ArrowRight className={''} />
-                                                    </div>
-                                                </Link>
-                                            </React.Fragment>
-                                        ) : item.showNotLoggedin ? (
-                                            <Link to={item.path} key={key}>
-                                                <div className=" w-100 px-24 py-3 d-flex justify-content-between align-items-center cursor-ointer">
-                                                    <div className="card-menu-name d-flex align-items-center">
-                                                        {item.icon}
-                                                        <h3 className="text-sm grey-text font-bold mb-0 ml-2">
-                                                            {item.name}
-                                                        </h3>
-                                                    </div>
-                                                    <ArrowRight className={''} />
-                                                </div>
-                                            </Link>
-                                        ) : (
-                                            ''
-                                        )}
-                                    </React.Fragment>
-                                ))}
-
-                            {userLoggedIn && (
-                                <div className="px-24 mt-5">
-                                    <button
-                                        onClick={handleLogout}
-                                        type="button"
-                                        className="btn btn-primary btn-mobile btn-block btn-outline">
-                                        Logout
-                                    </button>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
                 <div>
                     <div id="heros" className="content-container w-100 mb-3">
                         <Slider {...settings}>
