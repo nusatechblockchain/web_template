9a305eaad4f13dc28613512c9b7f5054f531961a
azharluthfi14 <azharluthfi4@gmail.com>
2022-12-28 11:18:02 +0700

    Fix layout internal transfer mobile

--- src/mobile/screens/InternalTransfer/index.tsx
+++ src/mobile/screens/InternalTransfer/index.tsx
@@ -61,8 +61,8 @@ const InternalTransferMobileScreen: React.FC = () => {
     }, [transferSuccess]);
 
     return (
-        <section className="internal-transfer-mobile-screen pb-5">
-            <div className="container-fluid w-100 p-0 m-0 dark-bg-main">
+        <section className="internal-transfer-mobile-screen pb-5 dark-bg-main">
+            <div className="container-fluid w-100 p-0 m-0">
                 <div className="mb-4">
                     <Link to="/wallets">
                         <ArrowLeft className="white-text" />
