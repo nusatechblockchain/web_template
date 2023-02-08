d7d68cdb2bf4b509ca5e1721f6ec9fb739b8acb6
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2023-02-08 13:24:02 +0700

    diy - fixing modal login session

--- src/components/ExpiredSessionModal/index.tsx
+++ src/components/ExpiredSessionModal/index.tsx
@@ -1,30 +1,48 @@
 import * as React from 'react';
 import { Button } from 'react-bootstrap';
+import { Modal } from 'src/desktop/components';
 
 export interface ExpiredSessionModalProps {
     title: string;
     buttonLabel: string;
+    show: boolean;
     handleSubmitExpSessionModal: () => void;
     handleChangeExpSessionModalState: () => void;
 }
 
 export class ExpiredSessionModal extends React.Component<ExpiredSessionModalProps> {
     public render() {
-        const { title, buttonLabel } = this.props;
+        const { title, buttonLabel, show } = this.props;
 
-        return (
-            <div className='card'>
-                <div onClick={this.props.handleChangeExpSessionModalState}>Close</div>
-                <Button
-                    block={true}
-                    type="button"
-                    onClick={this.props.handleSubmitExpSessionModal}
-                    size="lg"
-                    variant="primary"
-                >
-                    {buttonLabel}
-                </Button>
-            </div>
-        );
+        const renderModalLogin = () => {
+            return (
+                <React.Fragment>
+                    <h1 className="text-xl white-text font-semibold mb-24">{title}</h1>
+                    <div className="d-flex justify-content-between align-items-center gap-8 w-100">
+                        <Button
+                            block={true}
+                            type="button"
+                            onClick={this.props.handleSubmitExpSessionModal}
+                            size="lg"
+                            className="w-50"
+                            variant="primary">
+                            {buttonLabel}
+                        </Button>
+                        <Button
+                            block={true}
+                            type="button"
+                            size="lg"
+                            onClick={this.props.handleChangeExpSessionModalState}
+                            variant="outline-secondary m-0 text-sm font-semibold"
+                            style={{ height: '45px' }}
+                            className="w-50">
+                            Close
+                        </Button>
+                    </div>
+                </React.Fragment>
+            );
+        };
+
+        return <Modal show={true} content={renderModalLogin()} className="com-modal-expired-session" />;
     }
 }
