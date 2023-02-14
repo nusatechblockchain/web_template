3a4f60dada785455ca19da9b189a4e6a65bb97b9
diydiyydiyyy <chalimatus.sadiyah.098@gmail.com>
2023-02-14 14:19:24 +0700

    diy - announcement slider responseive

--- src/desktop/screens/AnnouncementScreen/index.tsx
+++ src/desktop/screens/AnnouncementScreen/index.tsx
@@ -35,6 +35,27 @@ export const AnnouncementScreen: FC = (): ReactElement => {
         autoplay: true,
         autoplaySpeed: 3000,
         pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 1020,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+        ],
     };
 
     return (
